4ad79d29e34710fe29ed72099718643dd0e0e8f9
David Baugh <114878979+baughtnet@users.noreply.github.com>
2023-05-07 23:12:10 -0700

    Added video title, labels indicating error or successful download and a progress bar

--- dl_gui.py
+++ dl_gui.py
@@ -1,3 +1,4 @@
+# downloads yt videos using pytube library, adapted from yt tutorial: https://www.youtube.com/watch?v=NI9LXzo0UY0
 import tkinter
 import customtkinter
 from pytube import YouTube
@@ -5,26 +6,30 @@ import ssl
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
-
-def startDownload():
-    # ytLink = link.get()
-    # ytObject = YouTube(ytLink)
-    # print(ytLink)
-    # print(ytObject)
-    # print(ytObject.vid_info)
-    # video = ytObject.streams.get_highest_resolution()
-    # if video is not None:
-        # video.download()
-    # else:
-        # print('Could not find high resolution video to download')
 # This portion was written from ChatGPT and pytube reference when I thought the program wasn't working
 # Turns out pytube was incorrectly installed, see https://github.com/pytube/pytube/issues/743 for details
-    url = link.get()
-    yt = YouTube(url)
-    yt.streams.get_highest_resolution().download()
-    print("Downloading...")
+# Removed original code, moving forward with this
+def startDownload():
+    try:
+        url = link.get()
+        yt = YouTube(url, on_progress_callback=on_progress)
+        video = yt.streams.get_highest_resolution()
+        title.configure(text=yt.title, text_color="white")
+        finishLabel.configure(text="")
+        video.download()
+        finishLabel.configure(text="Download Complete!", text_color="black")
+    except:
+        finishLabel.configure(text="Invalid Link", text_color="red")
 
-
+def on_progress(stream, chunk, bytes_remaining):
+    total_size = stream.filesize
+    bytes_downloaded = total_size - bytes_remaining
+    percentage_of_completion = bytes_downloaded / total_size * 100
+    print(percentage_of_completion)
+    per = str(int(percentage_of_completion))
+    pPercent.configure(text=per + '%')
+    pPercent.update()
+    progressBar.set(float(percentage_of_completion) / 100 )
 
 customtkinter.set_appearance_mode("System")
 customtkinter.set_default_color_theme("blue")
@@ -42,6 +47,21 @@ url_var = tkinter.StringVar()
 link = customtkinter.CTkEntry(app, width=350, height=40, textvariable=url_var)
 link.pack()
 
+# finished downloading
+finishLabel = customtkinter.CTkLabel(app, text="")
+finishLabel.pack()
+
+pPercent = customtkinter.CTkLabel(app, text="0%")
+pPercent.pack()
+
+progressBar = customtkinter.CTkProgressBar(app, width=400)
+progressBar.set(0.5)
+progressBar.pack(padx=10, pady=10)
+
 download = customtkinter.CTkButton(app, text="Download", command=startDownload)
 download.pack(padx=10, pady=10)
+
+quitApp = customtkinter.CTkButton(app, text="Quit", command=app.destroy)
+quitApp.pack()
+
 app.mainloop()
