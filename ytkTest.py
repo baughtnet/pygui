ded150fcf71816931bb670281de6decb747a1284
David Baugh <114878979+baughtnet@users.noreply.github.com>
2023-05-08 00:51:04 -0700

    code in ytkTest.py is fully working and ready to implement into project

--- ytkTest.py
+++ ytkTest.py
@@ -1,4 +1,5 @@
 from pytube import YouTube
+from io import BytesIO
 import ssl
 import urllib.request
 from PIL import Image, ImageTk
@@ -12,19 +13,23 @@ window.geometry("720x480")
 ssl._create_default_https_context = ssl._create_unverified_context
 # get the video url from the user
 # url = input("Enter the video URL: ")
-url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+url = "https://www.youtube.com/watch?v=pQrM5L6C-FQ"
 # get thumb url and video title
 yt = YouTube(url)
-image_url = yt.thumbnail_url
+image_url = yt.thumbnail_url.replace('default.jpg', 'hqdefault.jpg')
+print(image_url)
 yt_title = yt.title
 
-urllib.request.urlretrieve(image_url, "image.jpg")
+response = urllib.request.urlopen(image_url)
+image_data = response.read()
+# urllib.request.urlretrieve(image_url, "image.jpg")
+# image_data = Image.open("image.jpg")
+image = Image.open(BytesIO(image_data))
 
-pil_image = Image.open("image.jpg")
+image = image.resize((320, 180), Image.ANTIALIAS)
+yt_image = ImageTk.PhotoImage(image)
 
-yt_image = ImageTk.PhotoImage(pil_image)
-
-image_label = tk.Label(window, image=yt_image)
+image_label = tk.Label(window, width=640, height=360, image=yt_image)
 image_label.pack(padx=10, pady=10)
 
 title_label = tk.Label(window, text=yt_title)
