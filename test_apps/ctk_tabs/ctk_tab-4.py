f81be58ffccfd16af621cfaf040094a03823b9e5
David Baugh <114878979+baughtnet@users.noreply.github.com>
2023-05-10 19:15:25 -0700

    ctk_tab-4.py is ready to implement into the main project
    combobox will read in available video streams and the download button will download the selected resolution

--- test_apps/ctk_tabs/ctk_tab-4.py
+++ test_apps/ctk_tabs/ctk_tab-4.py
@@ -5,16 +5,36 @@ import customtkinter
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
-def printMe():
-    state = dropdown1.get()
-    print(state)
+itag_dict = {
+    "144p": "160",
+    "240p": "133",
+    "360p": "134",
+    "480p": "135",
+    "720p": "136",
+    "1080p": "137",
+    "1440p": "271",
+    "2160p": "313"
+}
+
+# function for downloading the selected video
+def download_video():
+    down_res = dropdown1.get()
+    print(down_res)
+    itag = itag_dict.get(down_res)
+    # itag = int(check_itag)
+    print(itag)
+    if not itag:
+        print("Invalid Resolution")
+    yt = YouTube(url)
+    video = yt.streams.get_by_itag(itag)
+    video.download()
+
 
 url = "https://www.youtube.com/watch?v=TpdapO9QGRo&t"
 yt = YouTube(url)
 
 # get available video qualities
 video_qualities = [stream.resolution for stream in yt.streams.filter(type="video", video_codec="vp9", progressive=False)]
-print(yt.streams.filter(type="video", progressive=False, video_codec="vp9"))
 
 # set up main window
 customtkinter.set_appearance_mode("System")
@@ -24,17 +44,16 @@ app = customtkinter.CTk()
 app.geometry("720x720")
 app.title("Dropdown Fill Test App")
 
+# combobox for resolution selection
 dropdown1 = customtkinter.CTkComboBox(app)
 dropdown1.pack()
 
 # update dropdown options with available video qualities
 dropdown1.configure(values=["Video Quality"] + video_qualities)
 
-download_button = customtkinter.CTkButton(app, command=printMe)
+# download button runs download_video function
+download_button = customtkinter.CTkButton(app, command=download_video)
 download_button.pack()
 state = dropdown1.cget("state")
 
-state2 = dropdown1.get()
-print(state)
-print(state2)
 app.mainloop()
--- test_apps/pytube/yt.py
+++ test_apps/pytube/yt.py
@@ -10,13 +10,28 @@ url = "https://www.youtube.com/watch?v=TpdapO9QGRo&t"
 yt = YouTube(url)
 # print(yt.vid_info)
 # show streams
+# for stream in yt.streams:
+    # print(stream)
+
+# url2 = "https://www.youtube.com/watch?v=gpP_YEv_9jA"
+
+# yt = YouTube(url2)
+# print("" + "\n" + "***********************************************************************" + "\n")
 for stream in yt.streams:
     print(stream)
+# qualities = [stream.resolution for stream in yt.streams.filter(type="video", progressive=True)]
+# print(qualities)
 
-url2 = "https://www.youtube.com/watch?v=gpP_YEv_9jA"
-
-yt = YouTube(url2)
-print("" + "\n" + "***********************************************************************" + "\n")
-for stream in yt.streams:
-    print(stream)
-
+# 144p - 160
+# 240p - 133
+# 360p - 134
+# 480 - 135
+# 720p - 136
+# 1080p - 137
+# 1440p - 271
+# 2160p - 313
+# 251 - hq webm audio(160) 
+# 250 - mq 70
+# 249 - lq 50
+# 140 - hq mp4(128)
+# 139 - lq 48
